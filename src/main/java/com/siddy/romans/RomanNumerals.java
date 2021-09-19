be9f885f2db8ef59537a6aaf420b5d24c00738af
Abi Sidford <asidford@gmail.com>
2021-09-19 17:12:18 +0100

    Add more tests and some implementations. One test fails.

--- src/main/java/com/siddy/romans/RomanNumerals.java
+++ src/main/java/com/siddy/romans/RomanNumerals.java
@@ -1,28 +1,75 @@
 package com.siddy.romans;
 
 public class RomanNumerals {
-    public String getNumeral(int number){
-        switch (number) {
-            case 1 :
-                return "I";
-            case 2:
-                return "II";
-            case 3:
-                return "III";
-            case 4:
-                return "IV";
-            case 5:
-                return "V";
-            case 6:
-                return "VI";
-            case 7:
-                return "VII";
-            case 8:
-                return "VIII";
-            case 9:
-                return "IX";
-            default:
-                return "ARGH!";
+
+    public String getNumeral(int number) {
+
+        // Get last digit
+        // Get the numeral
+        // Take last digit off number
+        int length = String.valueOf(number).length();
+        if (number == 0) {
+            return "";
         }
+        return switch (length) {
+            case 1 -> getUnit(number);
+            case 2 -> getTens(getLastDigit(stripOffLastDigit(number))) + getUnit(getLastDigit(number));
+            case 3 -> getHundreds(getLastDigit(stripOffLastDigit(stripOffLastDigit(number)))) + getTens(getLastDigit(stripOffLastDigit(number))) + getUnit(getLastDigit(number));
+            default -> "Something went wrong";
+        };
+
+    }
+
+    private int stripOffLastDigit(int number) {
+        return number / 10;
+    }
+
+    private int getLastDigit(int number) {
+        return number % 10;
+    }
+
+    private String getUnit(int number) {
+        return switch (number) {
+            case 1 -> "I";
+            case 2 -> "II";
+            case 3 -> "III";
+            case 4 -> "IV";
+            case 5 -> "V";
+            case 6 -> "VI";
+            case 7 -> "VII";
+            case 8 -> "VIII";
+            case 9 -> "IX";
+            default -> "";
+        };
+    }
+
+    private String getTens(int number) {
+        return switch (number) {
+            case 1 -> "X";
+            case 2 -> "XX";
+            case 3 -> "XXX";
+            case 4 -> "XL";
+            case 5 -> "L";
+            case 6 -> "LX";
+            case 7 -> "LXX";
+            case 8 -> "LXXX";
+            case 9 -> "XC";
+            default -> "";
+        };
+    }
+
+    private String getHundreds(int number) {
+        return switch (number) {
+            case 1 -> "C";
+            case 2 -> "CC";
+            case 3 -> "CCC";
+            case 4 -> "CD";
+            case 5 -> "D";
+            case 6 -> "DC";
+            case 7 -> "DCC";
+            case 8 -> "DCCC";
+            case 9 -> "CM";
+            default -> "";
+        };
     }
 }
--- src/test/java/com/siddy/romans/TestNumerals.java
+++ src/test/java/com/siddy/romans/TestNumerals.java
@@ -7,16 +7,6 @@ import static org.junit.Assert.*;
 public class TestNumerals {
 
     @Test
-    public void testGetsFive() {
-        // Arrange
-        RomanNumerals numerals = new RomanNumerals();
-        // Act
-        String result = numerals.getNumeral(5);
-        // Assert
-        assertEquals("V", result);
-    }
-
-    @Test
     public void testGetsOne() {
         // Arrange
         RomanNumerals numerals = new RomanNumerals();
@@ -27,12 +17,82 @@ public class TestNumerals {
     }
 
     @Test
-    public void testGetsNine() {
+    public void testGetNine(){
         // Arrange
         RomanNumerals numerals = new RomanNumerals();
         // Act
         String result = numerals.getNumeral(9);
         // Assert
         assertEquals("IX", result);
+    }
+
+    @Test
+    public void testGetZero(){
+        // Arrange
+        RomanNumerals numerals = new RomanNumerals();
+        // Act
+        String result = numerals.getNumeral(0);
+        // Assert
+        assertEquals("", result);
+    }
+
+    @Test
+    public void testGetFourteen(){
+        // Arrange
+        RomanNumerals numerals = new RomanNumerals();
+        // Act
+        String result = numerals.getNumeral(14);
+        // Assert
+        assertEquals("XIV", result);
+    }
+
+    @Test
+    public void testGetEightySix(){
+        // Arrange
+        RomanNumerals numerals = new RomanNumerals();
+        // Act
+        String result = numerals.getNumeral(86);
+        // Assert
+        assertEquals("LXXXVI", result);
+    }
+
+    @Test
+    public void testGetNinetyNine(){
+        // Arrange
+        RomanNumerals numerals = new RomanNumerals();
+        // Act
+        String result = numerals.getNumeral(99);
+        // Assert
+        assertEquals("XCIX", result);
+    }
+
+    @Test
+    public void testGetZeroThreeTwo(){
+        // Arrange
+        RomanNumerals numerals = new RomanNumerals();
+        // Act
+        String result = numerals.getNumeral(032);
+        // Assert
+        assertEquals("XXXII", result);
+    }
+
+    @Test
+    public void testGetOneHundredAndThree(){
+        // Arrange
+        RomanNumerals numerals = new RomanNumerals();
+        // Act
+        String result = numerals.getNumeral(103);
+        // Assert
+        assertEquals("CIII", result);
+    }
+
+    @Test
+    public void testGetNineHundredAndSeventy(){
+        // Arrange
+        RomanNumerals numerals = new RomanNumerals();
+        // Act
+        String result = numerals.getNumeral(970);
+        // Assert
+        assertEquals("CMLXX", result);
     }
 }
